c5eb6650ef63b229f20e34a934b2189d1a9e3a82
gigabbbyte <gabrielalammers@gmail.com>
2023-09-19 21:49:20 -0500

    HW 19 turn in

--- js/index.js
+++ js/index.js
@@ -17,3 +17,16 @@ function fade () {
 }
 
 setTimeout(fade, 3000)
+
+// cursor possibly
+// const cursor = document.querySelector('.cursor');
+
+// const positionElement = (e)=> {
+//   const mouseY = e.clientY;
+//   const mouseX = e.clientX;
+
+//   cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+// }
+
+// window.addEventListener('mousemove', positionElement)
